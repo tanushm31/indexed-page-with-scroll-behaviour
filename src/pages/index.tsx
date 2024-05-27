be6e53cd6c978fd246b688f2d674ebf7068ce456
Tanush Mahajan <tanush.mohina@gmail.com>
2024-05-27 17:36:43 +0530

    scrollable-demo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,258 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const getDimensions = (ele: HTMLDivElement) => {
+	// Get Height, Top and Bottom Offset of current element with respect to the parent
+	const parent = document.getElementById("content-container");
+	if (parent === null) return { height: 0, offsetTop: 0, offsetBottom: 0 };
+	const child = ele;
+	const bounderParent = parent.getBoundingClientRect();
+	const bounder = ele.getBoundingClientRect();
+	const height = bounder.height;
+	const offsetTop = bounder.top - bounderParent.top;
+	const offsetBottom = offsetTop + height;
+
+	return {
+		height,
+		offsetTop,
+		offsetBottom,
+	};
+};
+const scrollTo = (ele: any) => {
+	ele.scrollIntoView({
+		behavior: "smooth",
+		block: "start",
+	});
+};
+
+type IScectionComponent = {
+	header: string;
+	sectionID: string;
+	referenceVar: MutableRefObject<HTMLDivElement | null>;
+	children: React.ReactNode;
+};
+
+const SectionComponent = ({
+	header,
+	sectionID,
+	referenceVar,
+	children,
+}: IScectionComponent) => {
+	return (
+		<section id={sectionID} ref={referenceVar} className="flex flex-col w-full">
+			<h1 className="text-4xl font-bold">{header}</h1>
+			{children}
+		</section>
+	);
+};
+
+const ColumnSelection = () => {
+	return (
+		<div className="flex items-start justify-between overflow-x-auto ">
+			<div className="h-[600px] w-[400px] flex flex-col justify-between rounded border-2 border-black">
+				<select multiple={true} className="h-[94%]">
+					<option value={1}>1</option>
+					<option value={2}>2</option>
+					<option value={3}>3</option>
+					<option value={4}>4</option>
+				</select>{" "}
+				<button className="px-3 py-2 text-white bg-black rounded">
+					Add Selected
+				</button>
+			</div>
+			<div className="h-[600px] w-[400px] flex flex-col justify-between rounded border-2 border-black">
+				<select multiple={true} className="h-[94%]">
+					<option value={1}>1</option>
+					<option value={2}>2</option>
+					<option value={3}>3</option>
+					<option value={4}>4</option>
+				</select>{" "}
+				<button className="px-3 py-2 text-white bg-black rounded">
+					Remove Selected
+				</button>
+			</div>
+		</div>
+	);
+};
+const UserGroupSelection = () => {
+	return (
+		<div className="flex flex-col items-start justify-between space-y-3 overflow-x-auto ">
+			{/* Group Creation */}
+			<div className="flex justify-start w-full ">
+				<input
+					type="text"
+					placeholder="Group Name"
+					className="px-2 py-1 border-2 border-black rounded"
+				/>
+				<select className="ml-2 border-2 border-black">
+					<option value={1}>Priority 1</option>
+					<option value={2}>Priority 2</option>
+					<option value={3}>Priority 3</option>
+					<option value={4}>Priority 4</option>
+				</select>{" "}
+				<button className="px-3 py-2 ml-2 text-white bg-black rounded">
+					Create Group
+				</button>
+			</div>
+			{/* Available User Groups */}
+			<div className="flex flex-col w-full">
+				<div className="w-full h-[500px] border-2 rounded border-black">
+					Table Of Avaialable Groups
+				</div>
+				<button className="px-3 py-2 text-white bg-black rounded">
+					Edit Group
+				</button>
+			</div>
+			{/* Edit Group Section */}
+			<div className="flex flex-col justify-start w-full">
+				<div className="flex justify-between w-full">
+					<div className="h-[400px] w-[400px] flex flex-col justify-between rounded space-y-2">
+						<div>Users in Group</div>
+						<select multiple={true} className="h-[90%] border-2 border-black">
+							<option value={1}>1</option>
+							<option value={2}>2</option>
+							<option value={3}>3</option>
+							<option value={4}>4</option>
+						</select>{" "}
+						<button className="px-3 py-2 text-white bg-black rounded">
+							Remove Users From Group
+						</button>
+					</div>
+					<div className="h-[400px] w-[400px] flex flex-col justify-between rounded space-y-2">
+						<div>Users not in Group</div>
+						<select multiple={true} className="h-[90%] border-2 border-black">
+							<option value={1}>1</option>
+							<option value={2}>2</option>
+							<option value={3}>3</option>
+							<option value={4}>4</option>
+						</select>{" "}
+						<button className="px-3 py-2 text-white bg-black rounded">
+							Add Users To Group
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [visibleSection, setVisibleSection] = useState<string | undefined>(
+		undefined
+	);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const headerRef = useRef(null);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	//  Refs for the index-section headings
+	const indexSection1Ref = useRef(null);
+	const indexSection2Ref = useRef(null);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	//  Refs for the sections
+	const sectionRefs = [
+		{ section: "column-selection", ref: indexSection1Ref },
+		{ section: "user-group-selection", ref: indexSection2Ref },
+	];
+	// const section1Ref = useRef(null);
+	// const section2Ref = useRef(null);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+	useEffect(() => {
+		// .log("HELLO");
+		// handle Scrolling function
+		const contentContainer = document.getElementById(
+			"content-container"
+		) as HTMLDivElement;
+		if (contentContainer) {
+			const handleScroll = () => {
+				// .log("handleScroll init");
+				//  Get the current scroll position
+				// const { height: headerHeight } = getDimensions(headerRef.current);
+				const scrollPosition = contentContainer.scrollTop;
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+				const selected = sectionRefs.find(({ section, ref }) => {
+					const ele = ref.current;
+					if (ele) {
+						const { offsetBottom, offsetTop } = getDimensions(ele);
+						// .log({
+						// 	scrollPosition: scrollPosition,
+						// 	offsetTop: offsetTop,
+						// 	offsetBottom: offsetBottom,
+						// });
+						return scrollPosition > offsetTop && scrollPosition < offsetBottom;
+					}
+				});
+
+				if (selected && selected.section !== visibleSection) {
+					setVisibleSection(selected.section);
+				} else if (!selected && visibleSection) {
+					setVisibleSection(undefined);
+				}
+				// .log("Visible Section:", visibleSection);
+			};
+			handleScroll();
+
+			contentContainer.addEventListener("scroll", handleScroll);
+			return () => {
+				contentContainer.removeEventListener("scroll", handleScroll);
+			};
+		}
+		// if
+	}, []);
+	return (
+		<main
+			className={` h-screen w-screen flex bg-white min-h-screen flex-col items-center justify-between pt-5 pl-24 ${inter.className}`}
+		>
+			<div className="flex justify-between w-full h-full p-2 ">
+				<div
+					id="content-container"
+					className="w-full h-full space-y-2 space-y-10 overflow-y-scroll "
+				>
+					<SectionComponent
+						header="Column Selection"
+						sectionID="column-selection"
+						referenceVar={indexSection1Ref}
+					>
+						<ColumnSelection />
+					</SectionComponent>
+					<SectionComponent
+						header="User Group Selection"
+						sectionID="user-group-selection"
+						referenceVar={indexSection2Ref}
+					>
+						<UserGroupSelection />
+					</SectionComponent>
+				</div>
+				<div
+					ref={headerRef}
+					id="index-section"
+					className="h-full w-[300px] pl-5"
+				>
+					<ul className="">
+						<li
+							onMouseDown={() => {
+								scrollTo(indexSection1Ref.current);
+							}}
+							className={`hover:text-blue-700 hover:cursor-pointer ${
+								visibleSection === "column-selection" ? "font-bold" : ""
+							}`}
+						>
+							Section 1
+						</li>
+						<li
+							onMouseDown={() => {
+								scrollTo(indexSection2Ref.current);
+							}}
+							className={`hover:text-blue-700 hover:cursor-pointer ${
+								visibleSection === "user-group-selection" ? "font-bold" : ""
+							}`}
+						>
+							Section 2
+						</li>
+					</ul>
+				</div>
+			</div>
+		</main>
+	);
 }
